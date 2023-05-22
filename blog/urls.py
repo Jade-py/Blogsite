92fcff660087a4c1510792a67f0476cbac75b717
Justin George <justingeorge322@gmail.com>
2023-05-22 23:54:45 +0530

    updated urls

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('post/<int:pk>/', blog_post.as_view(), name='blog'),
     path('post/new/', newBlogView.as_view(), name='newBlog'),
     path('post/<int:pk>/edit/', updateBlogView.as_view(), name='editBlog'),
-    path('post/<int:pk>/delete/', deleteBlogView.as_view(), name='delete                                                                                                                                                                                                                                                                                                                                                                            gitBlog'),
+    path('post/<int:pk>/delete/', deleteBlogView.as_view(), name='deleteBlog'),
 
 ]
